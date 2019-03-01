9262f9cb81af041c4e370ef81ca3ec29b71d831f
JoaoNSE <nobreenrique@hotmail.com>
2019-03-01 08:33:59 -0300

    Initial commit

---
+++ src/main/java/com/crp/qxd/StoryShow/StoryShowApplication.java
@@ -0,0 +1,13 @@
+package com.crp.qxd.StoryShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryShowApplication.class, args);
+	}
+
+}
