6e1a91d025c695a8cca8bde66d657f2b3cdd0b79
JoaoNSE <nobreenrique@hotmail.com>
2019-03-01 14:36:23 -0300

    Upload de imagens funcionando

--- src/main/java/com/crp/qxd/StoryShow/StoryShowApplication.java
+++ src/main/java/com/crp/qxd/StoryShow/StoryShowApplication.java
@@ -1,14 +1,28 @@
 package com.crp.qxd.StoryShow;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.crp.qxd.StoryShow.storage.StorageProperties;
+import com.crp.qxd.StoryShow.storage.StorageService;
 
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class StoryShowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoryShowApplication.class, args);
 	}
+	
+	@Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.init();
+        };
+    }
 
 }
